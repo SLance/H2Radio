bf3e434b9627878764396c815f83d3dc84e9560f
兰超 <lanchao0713@msn.com>
2015-09-16 11:43:15 +0800

    初始化xcode项目

---
+++ H2Radio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  H2Radio
+//
+//  Created by Lance on 15/9/16.
+//  Copyright © 2015年 Lance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
